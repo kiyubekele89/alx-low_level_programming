e42b936a46d5eecd40b9439866b17cff9a11f9b3
kiyubekele89 <118066707+kiyubekele89@users.noreply.github.com>
2022-12-09 21:26:52 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,40 @@
 /*
- * auth: eyarios bekele
- *
  * date: December 9, 2022
+ * Auth: eyarios bekele
  */
+
 #include <stdio.h>
+
 /**
- * main -  prints all possible different combinations of three digits
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
  *
- * Return: Always 0(Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	/* local variable declaration */
-	int firstDigit;
-	int secondDigit;
-	int lastDigit;
+	int digit1, digit2, digit3;
 
-	for (firstDigit = 0; firstDigit < 8; firstDigit++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (secondDigit = 1; secondDigit < 9; secondDigit++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			for (lastDigit = 2; lastDigit < 10; lastDigit++)
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
-				if (firstDigit == secondDigit)
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
-				if (secondDigit == lastDigit)
-					continue;
-				if (firstDigit == lastDigit)
-					continue;
-				putchar((firstDigit % 10) + '0');
-				putchar((secondDigit % 10) + '0');
-				putchar((lastDigit % 10) + '0');
-				if (firstDigit == 7 && secondDigit == 8 && lastDigit == 9)
-					continue;
+				
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
