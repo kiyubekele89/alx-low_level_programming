87b38570f6a809cf12137e85300d8f75e6b2f469
kiyubekele89 <kiyubekele87@gmail.com>
2023-04-26 12:50:54 +0300

    completed a task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,12 @@
  */
 int main(void)
 {
+  int result;
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+  if (result == -1) // Check for errors
+    {
+        perror("write"); // Print an error message
+        return 1; // Exit with a non-zero code
+    }
 	return (1);
 }
