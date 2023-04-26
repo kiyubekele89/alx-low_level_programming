41d6054ec00da0bb8c4f1cb070dfc892050a3b4b
kiyubekele89 <kiyubekele87@gmail.com>
2023-04-26 13:34:02 +0300

    completed a task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,19 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point
+ * 
+ * Return: always 0(Success)
+ */
+
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *mes = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 size_t length = 59;
-ssize_t result = write(2, message, length);
-if (result == -1)
+ssize_t result = write(2, mes, length);
+if (result == 1)
 {
 return (1);
 }
-return (0);
+return (1);
 }
