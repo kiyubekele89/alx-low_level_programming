3abbe6bbfe7f5b18c792ce78c0f23fa421c2e383
kiyubekele89 <kiyubekele87@gmail.com>
2023-04-26 13:27:40 +0300

    completed a task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 int main(void)
 {
-    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int length = 60;
-    int result = write(2, message, length);
-    if (result == -1)
-    {
-        return (1);
-    }
-    return (0);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t length = 59;
+ssize_t result = write(2, message, length);
+if (result == -1)
+{
+return (1);
+}
+return (0);
 }
