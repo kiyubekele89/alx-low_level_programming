64598f74a9280952c0462a17721ad02ffb453566
kiyubekele89 <kiyubekele87@gmail.com>
2023-04-26 12:39:46 +0300

    completed a task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0(Success)
  */
 int main(void)
 {
-  char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-  write(2,str,59);
-  return (1);
+	write(2,and that piece of art is useful\" - Dora Korpar, 2015-10-19
+,59);
+	return (1);
 }
