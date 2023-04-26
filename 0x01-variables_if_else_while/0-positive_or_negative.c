6bec44a938345b2a7c92179b72c07961f03d4ba4
kiyubekele89 <kiyubekele87@gmail.com>
2023-04-26 20:24:56 +0300

    completed a task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,21 +9,26 @@
  */
 int main(void)
 {
+	/* variable declaration */
 	int n;
 
+	/* variable initialization */
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* checks for pos, zero, neg */
+	/* checks for pos, zero, neg of a num */
 	if (n > 0)
 	{
+		/* print if the num is "positive" */
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
+		/* print if the num is "zero" */
 		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
+		/* print if the num is "negative" */
 		printf("%d is negative\n", n);
 	}
 	return (0);
