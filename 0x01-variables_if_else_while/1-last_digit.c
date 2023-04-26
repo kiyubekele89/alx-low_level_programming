128b90fc67369dfb184cfb7222ab7d97a0c4ede4
kiyubekele89 <kiyubekele87@gmail.com>
2023-04-26 21:43:35 +0300

    completed a task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,31 +9,30 @@
  */
 int main(void)
 {
-int n;
-int num;
+	int n;
+	int num;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+    /* last digit of n */
+	num = n % 10;
 
-/* last digit of n */
-num = n % 10;
-
-/* checks and print the last digit */
-if (num > 5)
-{
-/* print if the last digit is greater than 5 */
-printf("Last digit of %d is %d and is greater than 5\n", n, num);
-}
-/* if the above condition is not true execute this */
-else if (num == 0)
-{
-/* if the last digit is equal to 0 print this */
-printf("Last digit of %d is %d and is 0\n", n, num);
-}
-else if (num < 6 && num != 0)
-{
-/* if the last digit is < 6 & != 0 print this */
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
-}
-return (0);
+    /* checks and print the last digit */
+		if (num > 5)
+		{
+		/* print if the last digit is greater than 5 */
+			printf("Last digit of %d is %d and is greater than 5\n", n, num);
+		}
+		/* if the above condition is not true execute this */
+		else if (num == 0)
+		{
+			/* if the last digit is equal to 0 print this */
+			printf("Last digit of %d is %d and is 0\n", n, num);
+		}
+		else if (num < 6 && num != 0)
+		{
+			/* if the last digit is < 6 & != 0 print this */
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
+		}
+		return (0);
 }
