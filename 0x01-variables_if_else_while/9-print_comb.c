6519da6099310b73ca0167cafb937e9ec02d868e
kiyubekele89 <kiyubekele87@gmail.com>
2023-04-28 07:55:20 +0300

    completed a task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,15 +8,15 @@
 int main(void)
 {
 	/* variable declaration */
-	char digit;
+	int digit;
 
 	/* loop through the digits from 0 to 9 */
-	for (digit = '0'; digit <= '9'; digit++)
+	for (digit = 0; digit < 10; digit++)
 	{
 		/* print the current digit */
-		putchar(digit);
+		putchar(digit + '0');
 		/* checks if it not equal to 9 */
-		if (digit != '9')
+		if (digit != 9)
 		{
 			/* print a white space */
 			putchar(' ');
